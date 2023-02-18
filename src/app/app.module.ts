6b0ff1a831834357323a65c7cc0a08c734b2f1e3
saint-rivers <eam.dayan@gmail.com>
2023-02-18 09:17:45 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MinefieldComponent } from './components/minefield/minefield.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MinefieldComponent
   ],
   imports: [
     BrowserModule,
